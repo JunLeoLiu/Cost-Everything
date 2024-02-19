ba92a9a2584b3df51e4c690d8c5c16a9f696d077
JunLeoLiu <liujun2015@hotmail.com>
2024-02-19 15:00:35 +0800

    na

--- themes/ce/components/Footer.js
+++ themes/ce/components/Footer.js
@@ -10,7 +10,7 @@ export const Footer = (props) => {
   return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
         <DarkModeButton className='text-center pt-4'/>
 
-        <div className="container mx-auto w-fall py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+        <div className="container relative w-fall py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
             <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
             <div className="md:p-0 text-center md:text-right text-xs">
                 {/* 右侧链接 */}
