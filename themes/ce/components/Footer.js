468d1fbdf84471b025737907a2cc7f0dcf5409ed
JunLeoLiu <liujun2015@hotmail.com>
2024-02-19 15:23:08 +0800

    na

--- themes/ce/components/Footer.js
+++ themes/ce/components/Footer.js
@@ -9,14 +9,6 @@ export const Footer = (props) => {
 
   return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
         <DarkModeButton className='text-center pt-4'/>
-
-        <div className="z-10 relative w-fall py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
             <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
-            <div className="md:p-0 text-center md:text-right text-xs">
-                {/* 右侧链接 */}
-                {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-                {siteConfig('BEI_AN') && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{siteConfig('BEI_AN')} </a>)}
-            </div>
-        </div>
     </footer>
 }
