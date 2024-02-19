81e4aa99d40f2833ff35e6303eed4fe457f82cae
JunLeoLiu <liujun2015@hotmail.com>
2024-02-19 17:10:49 +0800

    na

--- themes/ce/config.js
+++ themes/ce/config.js
@@ -3,11 +3,8 @@
  */
 const CONFIG = {
   // 菜单配置
-  CE_MENU_CATEGORY: false, // 显示分类
-  CE_MENU_TAG: false, // 显示标签
-  CE_MENU_ARCHIVE: false, // 显示归档
-  CE_MENU_SEARCH: false, // 显示搜索
-
+  CE_MENU_SEARCH: true, // 显示搜索
+  
   CE_POST_LIST_COVER: true // 列表显示文章封面
 
 }
