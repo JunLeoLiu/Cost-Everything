5973df1757c85dae2d1a8fbb581b02aa16928e94
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 14:34:05 +0800

    na

--- themes/ce/components/TopNav.js
+++ themes/ce/components/TopNav.js
@@ -136,7 +136,9 @@ return (
     <div id='sticky-nav' style={{ backdropFilter: 'none' }} className={'duration-300 transition-all shadow-none bg-transparent dark:bg-transparent dark:text-gray-200 text-black w-full z-20 border-none dark:border-none'}>
       <div className='w-full flex justify-between items-center px-4 py-2'>
         <div className='flex'>
-          <img src='avatar.png' alt='Your Logo' className='w-10 h-10 mr-2' /> {/* 添加的图片 */}
+          <a href="/"> {/* 添加的链接 */}
+            <img src='avatar.png' alt='Your Logo' className='w-10 h-10 mr-2' /> {/* 添加的图片 */}
+          </a>
           <Logo {...props} />
         </div>
 
@@ -156,7 +158,6 @@ return (
     </SideBarDrawer>
   </div>
 )
-
 
 
 }
