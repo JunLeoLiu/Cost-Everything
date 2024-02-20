8c1a2423ec8d22daa2ad57aaa191bb90f2c1e292
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 10:34:52 +0800

    na

--- themes/ce/components/Title.js
+++ themes/ce/components/Title.js
@@ -9,12 +9,8 @@ import { siteConfig } from '@/lib/config'
 export const Title = (props) => {
   const { post } = props
   const title = post?.title || siteConfig('TITLE')
-  const description = post?.description || siteConfig('AUTHOR')
 
   return <div className="text-center px-6 py-12 mb-6 bg-gray-20 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
         <h1 className="text-xl md:text-4xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
-        <p className="leading-loose text-gray-dark">
-            {description}
-        </p>
     </div>
 }
