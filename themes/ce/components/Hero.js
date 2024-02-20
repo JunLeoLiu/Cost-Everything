946152cc73194cc6dceb85a55b5c62eb306a2f21
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 16:48:33 +0800

    na

--- themes/ce/components/Hero.js
+++ themes/ce/components/Hero.js
@@ -56,9 +56,9 @@ const Hero = props => {
             className="w-full h-screen relative bg-black"
         >
 
-            <div className="text-white absolute bottom-100 flex flex-col h-full items-center justify-center w-full ">
+            <div className="text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full ">
                 {/* 站点标题 */}
-                <div className='font-black text-4xl md:text-5xl shadow-text' style={{ bottom: '-20px' }}>{siteConfig('TITLE')}</div>
+                <div className='font-black text-4xl md:text-5xl shadow-text'>{siteConfig('TITLE')}</div>
                 {/* 站点欢迎语 */}
                 <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
                     <span id='typed' />
