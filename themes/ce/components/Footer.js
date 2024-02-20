51ee989f846c8452b13d6ac7d00c7125bdde8014
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 09:58:06 +0800

    na

--- themes/ce/components/Footer.js
+++ themes/ce/components/Footer.js
@@ -7,7 +7,7 @@ export const Footer = (props) => {
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
-  return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
+  return <footer className="z-10 relative w-full bg-white px-6 py-4 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
         <DarkModeButton className='text-center pt-4'/>
             <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
     </footer>
