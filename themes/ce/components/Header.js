a3facf5c52241bd88294b3dfbbfdcf112d39e3cd
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 11:57:26 +0800

    na

--- themes/ce/components/Header.js
+++ themes/ce/components/Header.js
@@ -8,8 +8,8 @@ import { siteConfig } from '@/lib/config'
 export const Header = (props) => {
   return (
 <header className="w-full px-6 bg-white dark:bg-black relative z-50 top-0 left-0 sticky">
-    <div className="container mx-auto max-w-2xl md:flex justify-left items-center">
-        <img src="/logofull.png" alt="Header Image" className="w-4 h-4 md:w-auto md:h-auto" />
+    <div className="container mx-auto max-w-4xl md:flex justify-start items-center">
+        <img src="/images/header-image.png" alt="Header Image" style={{ width: '100px', height: '100px' }} className="mr-4 md:mr-0 md:w-auto md:h-auto" />
         <div className="w-full md:w-auto text-center md:text-right">
             {/* 右侧文字 */}
         </div>
