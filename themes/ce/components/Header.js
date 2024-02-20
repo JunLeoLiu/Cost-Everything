9aac8db089936dca87204285963578276c03caf3
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 11:41:10 +0800

    na

--- themes/ce/components/Header.js
+++ themes/ce/components/Header.js
@@ -1,24 +1,8 @@
-import Link from 'next/link'
-import { siteConfig } from '@/lib/config'
-
-/**
- * 网站顶部
- * @returns
- */
-export const Header = (props) => {
-  return (
-      <header className="w-full px-6 bg-white dark:bg-black relative z-999999">
-            <div className="container mx-auto max-w-4xl md:flex justify-left items-center">
-                <Link
-                    href='/'
-                    className="py-2 px-6 w-full text-center md:text-left md:w-auto text-gray-dark no-underline flex justify-center items-center text-2xl font-bold">
-
-                    {siteConfig('TITLE')}
-                </Link>
-                <div className="w-full md:w-auto text-center md:text-right">
-                    {/* 右侧文字 */}
-                </div>
-            </div>
-        </header>
-  )
-}
+<header className="w-full px-6 bg-white dark:bg-black relative z-50 top-0 left-0 sticky">
+    <div className="container mx-auto max-w-4xl md:flex justify-center items-center">
+        <img src="/logofull.png" alt="Header Image" className="w-16 h-16 md:w-auto md:h-auto" />
+        <div className="w-full md:w-auto text-center md:text-right">
+            {/* 右侧文字 */}
+        </div>
+    </div>
+</header>
