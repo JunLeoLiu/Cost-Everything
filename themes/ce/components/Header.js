3c4a59ce1c50f075763812c73508eba9650ffc1b
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 12:09:23 +0800

    na

--- themes/ce/components/Header.js
+++ themes/ce/components/Header.js
@@ -9,7 +9,7 @@ export const Header = (props) => {
   return (
 <header className="w-full px-6 bg-white dark:bg-black relative z-50 top-0 left-0 sticky">
     <div className="container mx-auto max-w-4xl md:flex justify-start items-center">
-        <img src="/logofull.png" alt="Header Image" style={{ width: '100px', height: '100px' }} className="mr-4 md:mr-0 md:w-auto md:h-auto" />
+        <img src="/logofull.png" alt="Header Image" style={{ width: '100px', height: '80px' }} className="mr-4 md:mr-0 md:w-auto md:h-auto" />
         <div className="w-full md:w-auto text-center md:text-right">
             {/* 右侧文字 */}
         </div>
