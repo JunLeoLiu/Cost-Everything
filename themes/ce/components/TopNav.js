3d6bffd6956fbc3fafdd9c6476148186fc5aa2e7
JunLeoLiu <liujun2015@hotmail.com>
2024-02-20 15:15:14 +0800

    na

--- themes/ce/components/TopNav.js
+++ themes/ce/components/TopNav.js
@@ -138,6 +138,7 @@ return (
         <div className='flex'>
           <a href="/"> {/* 添加的链接 */}
             <img src='avatar.png' alt='Your Logo' className='w-10 h-10 mr-2' /> {/* 添加的图片 */}
+            <span className="font-bold">Your Logo</span> {/* 修改的文字 */}
           </a>
           <Logo {...props} />
         </div>
