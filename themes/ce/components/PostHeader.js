74f9a5ba293e2ae32d263b2ae2cf0209e2feaf36
JunLeoLiu <liujun2015@hotmail.com>
2024-02-21 18:49:01 +0800

    na

--- themes/ce/components/PostHeader.js
+++ themes/ce/components/PostHeader.js
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import TagItemMini from './TagItemMini'
 import { useGlobal } from '@/lib/global'
 import NotionIcon from '@/components/NotionIcon'
 import LazyImage from '@/components/LazyImage'
@@ -28,16 +27,6 @@ export default function PostHeader({ post, siteInfo }) {
             className="bg-black bg-opacity-70 absolute top-0 w-full h-48 py-10 flex justify-center items-center ">
 
         <div className='mt-10'>
-            <div className='mb-3 flex justify-center'>
-              {post.category && <>
-                <Link href={`/category/${post.category}`} passHref legacyBehavior>
-                  <div className="cursor-pointer px-2 py-1 mb-2 border rounded-sm dark:border-white text-sm font-medium hover:underline duration-200 shadow-text-md text-white">
-                    {post.category}
-                  </div>
-                </Link>
-              </>}
-            </div>
-
           {/* 文章Title */}
           <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center text-white">
             <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />{post.title}
@@ -68,16 +57,6 @@ export default function PostHeader({ post, siteInfo }) {
               {locale.COMMON.VIEWS}
             </div>}
           </section>
-
-            <div className='mt-4 mb-1'>
-                {post.tagItems && (
-                    <div className="flex justify-center flex-nowrap overflow-x-auto">
-                        {post.tagItems.map(tag => (
-                            <TagItemMini key={tag.name} tag={tag} />
-                        ))}
-                    </div>
-                )}
-            </div>
         </div>
       </header>
     </div>
