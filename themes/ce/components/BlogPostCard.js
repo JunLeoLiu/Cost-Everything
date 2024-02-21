f535e2f3688664b981a05ad3cd3719c362e25a0a
JunLeoLiu <liujun2015@hotmail.com>
2024-02-21 17:14:11 +0800

    na

--- themes/ce/components/BlogPostCard.js
+++ themes/ce/components/BlogPostCard.js
@@ -15,32 +15,35 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   const url = checkContainHttp(post.slug) ? sliceUrlFromHttp(post.slug) : `${siteConfig('SUB_PATH', '')}/${post.slug}`
 
   return (
-    <div className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`} >
-      <div key={post.id}
-        data-aos="fade-up"
-        data-aos-easing="ease-in-out"
-        data-aos-duration="800"
-        data-aos-once="false"
-        data-aos-anchor-placement="top-bottom"
-        id='blog-post-card'
-        className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
-          overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
 
-        {/* 文字内容 */}
-        <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary} />
+        <div className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`} >
+            <div key={post.id}
+                data-aos="fade-up"
+                data-aos-easing="ease-in-out"
+                data-aos-duration="800"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+                id='blog-post-card'
+                className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
+                    overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
 
-        {/* 图片封面 */}
-        {showPageCover && (
-          <div className="md:w-5/12 overflow-hidden">
-            <Link href={url} passHref legacyBehavior>
-              <a>
-                <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
-              </a>
-            </Link>
-          </div>
-        )}
+                {/* 文字内容 */}
+                <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary} />
 
-      </div>
-    </div>
+                {/* 图片封面 */}
+                {showPageCover && (
+                    <div className="md:w-5/12 overflow-hidden">
+                        <Link href={url} passHref legacyBehavior>
+                        <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
+                        </Link>
+                    </div>
+                )}
+
+            </div>
+
+        </div>
+
   )
 }
+
+export default BlogPostCard
