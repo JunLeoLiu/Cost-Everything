749df3e111c5b91c310ffe43ac1d99b1e789afa1
JunLeoLiu <liujun2015@hotmail.com>
2024-02-21 15:51:53 +0800

    na

--- themes/ce/components/ArticleCopyright.js
+++ themes/ce/components/ArticleCopyright.js
@@ -22,8 +22,8 @@ export default function ArticleCopyright () {
     <section className="dark:text-gray-300 mt-6 mx-1 ">
       <ul className="overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500">
         <li>
-          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <Link href={'/about'} className="hover:underline">
+          <strong className='mr-2'>{'来源'}:</strong>
+          <Link href={'https://www.costeverything.com/'} className="hover:underline">
             {siteConfig('AUTHOR')}
           </Link>
         </li>
