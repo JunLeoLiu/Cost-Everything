2687b5c60608f1cc2010955aad55280eb6d20134
JunLeoLiu <liujun2015@hotmail.com>
2024-02-22 17:20:02 +0800

    na

--- themes/ce/components/PostHeader.js
+++ themes/ce/components/PostHeader.js
@@ -20,38 +20,40 @@ export default function PostHeader({ post, siteInfo }) {
   const headerImage = post?.pageCover ? post.pageCover : siteInfo?.pageCover
 
   return (
-    <div id="header" className="w-full relative md:flex-shrink-0 z-10 flex flex-wrap items-center justify-center">
-      <header
-        id='article-header-cover'
-        className="bg-black bg-opacity-70 w-full md:w-2/3 h-80 py-10 flex flex-wrap items-center justify-center md:absolute md:right-0"
-      >
-        {/* 文章Title */}
-        <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md text-white text-center md:text-right md:w-2/3">
-          <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />{post.title}
-        </div>
+    <div id="header" className="w-full h-48 relative md:flex-shrink-0 z-10" >
+      <LazyImage priority={true} src={headerImage} className='w-full h-full object-cover object-center absolute top-0'/>
 
-        <LazyImage priority={true} src={headerImage} className='w-full h-30 object-cover object-center md:w-1/3'/>
-      </header>
+      <header id='article-header-cover'
+            className="bg-black bg-opacity-70 absolute top-0 w-full h-48 py-10 flex justify-center items-center ">
 
-      <section className="flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8">
-        <div className='flex justify-center dark:text-gray-200 text-opacity-70'>
-          {post?.type !== 'Page' && (
-            <>
-              <span className="pl-1 mr-2">
-                {locale.COMMON.POST_TIME}: {post?.publishDay}
-              </span>
-            </>
-          )}
-          <div className="pl-1 mr-2">
-            {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
+        <div className='mt-10'>
+          {/* 文章Title */}
+          <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center text-white">
+            <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />{post.title}
           </div>
-        </div>
 
-        {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && <div className="busuanzi_container_page_pv font-light mr-2">
-          {locale.COMMON.VIEWS}
-          <span className="mr-2 busuanzi_value_page_pv" />
-        </div>}
-      </section>
+          <section className="flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8">
+
+            <div className='flex justify-center dark:text-gray-200 text-opacity-70'>
+              {post?.type !== 'Page' && (
+                <>
+                  <span className="pl-1 mr-2">
+                    {locale.COMMON.POST_TIME}: {post?.publishDay}
+                  </span>
+                </>
+              )}
+              <div className="pl-1 mr-2">
+                {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
+              </div>
+            </div>
+
+            {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && <div className="busuanzi_container_page_pv font-light mr-2">
+              {locale.COMMON.VIEWS}
+              <span className="mr-2 busuanzi_value_page_pv" />
+            </div>}
+          </section>
+        </div>
+      </header>
     </div>
   )
 }
