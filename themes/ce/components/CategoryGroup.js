c17aa961fb5808630129ae6be5bf8dd76bc4311d
JunLeoLiu <liujun2015@hotmail.com>
2024-02-22 14:35:00 +0800

    na

--- themes/ce/components/CategoryGroup.js
+++ themes/ce/components/CategoryGroup.js
@@ -1,13 +1,19 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const CategoryGroup = ({ currentCategory, categories }) => {
-  if (!categories) {
-    return <></>
+const CategoryGroup = ({ currentCategory, categories, showAll=1}) => {
+  // 添加一个“全部”类别
+  if (showAll) {
+    categories = [{ name: "全部", count: categories.reduce((total, category) => total + category.count, 0) }, ...categories];
   }
-  return <>
-    <div id='category-list' className='dark:border-gray-600 flex flex-wrap  mx-4'>
+
+  if (!categories) {
+    return <></>;
+  }
+
+  return (
+    <div id='category-list' className='dark:border-gray-600 flex flex-wrap mx-4'>
       {categories.map(category => {
-        const selected = currentCategory === category.name
+        const selected = currentCategory === category.name;
         return (
           <Link
             key={category.name}
@@ -16,7 +22,7 @@ const CategoryGroup = ({ currentCategory, categories }) => {
             className={(selected
               ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
               : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
-              '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
+              ' text-sm w-full items-center duration-300 px-2 cursor-pointer py-1 font-light'}>
 
             <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})</div>
 
@@ -24,7 +30,7 @@ const CategoryGroup = ({ currentCategory, categories }) => {
         );
       })}
     </div>
-  </>;
-}
+  );
+};
 
-export default CategoryGroup
+export default CategoryGroup;
