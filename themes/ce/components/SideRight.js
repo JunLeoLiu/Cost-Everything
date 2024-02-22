47b84504bf96d4b9ee0fe48eab0698c6330fa4a9
JunLeoLiu <liujun2015@hotmail.com>
2024-02-22 14:02:55 +0800

    na

--- themes/ce/components/SideRight.js
+++ themes/ce/components/SideRight.js
@@ -55,10 +55,25 @@ export default function SideRight(props) {
           <div className='ml-2 mb-1 '>
             <i className='fas fa-th' /> {locale.COMMON.CATEGORY}
           </div>
-          <CategoryGroup
-            currentCategory={currentCategory}
-            categories={categories}
-          />
+          <div id="category-list" className="duration-200 flex flex-wrap mx-8">
+            {categoryOptions?.map(category => (
+              <Link
+                key={category.name}
+                href={`/category/${category.name}`}
+                passHref
+                legacyBehavior
+              >
+                <div
+                  className={
+                    'duration-300 dark:hover:text-white rounded-lg px-5 cursor-pointer py-2 hover:bg-indigo-400 hover:text-white'
+                  }
+                >
+                  <i className="mr-4 fas fa-folder" />
+                  {category.name}({category.count})
+                </div>
+              </Link>
+            ))}
+          </div>
         </Card>
       )}
 
