c649c4074b7dc037077a82d8906651123c9ffa41
JunLeoLiu <liujun2015@hotmail.com>
2024-02-22 14:21:16 +0800

    na

--- themes/ce/components/InfoCard.js
+++ themes/ce/components/InfoCard.js
@@ -15,11 +15,13 @@ export function InfoCard(props) {
 
   return (
     <Card className={className}>
-      <a href="/" className="icon-wrapper" style={{ height: '3.3rem', width: '3.8rem', backgroundColor: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ height: '2.8rem', width: '3.3rem', border: 'solid 3px #333', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <span style={{ fontFamily: 'Helvetica Neue', fontSize: '2rem', fontWeight: 'bold', color: '#000' }}>CE</span>
-        </div>
-      </a>
+      <div style={{ display: 'flex', justifyContent: 'center' }}> {/* 添加的 flex 容器 */}
+        <a href="/" className="icon-wrapper" style={{ height: '3.3rem', width: '3.8rem', backgroundColor: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <div style={{ height: '2.8rem', width: '3.3rem', border: 'solid 3px #333', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <span style={{ fontFamily: 'Helvetica Neue', fontSize: '2rem', fontWeight: 'bold', color: '#000' }}>CE</span>
+          </div>
+        </a>
+      </div>
       <div className='font-medium text-center text-xl pb-4'>{siteConfig('AUTHOR')}</div>
       <div className='text-sm text-center'>{siteConfig('BIO')}</div>
       <MenuGroupCard {...props} />
