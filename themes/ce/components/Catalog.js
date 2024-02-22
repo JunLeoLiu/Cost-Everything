05eb5e174116249d6c6ffc289d83a3ec5b94945a
JunLeoLiu <liujun2015@hotmail.com>
2024-02-22 17:42:51 +0800

    na

--- themes/ce/components/Catalog.js
+++ themes/ce/components/Catalog.js
@@ -4,15 +4,9 @@ import { uuidToId } from 'notion-utils'
 import Progress from './Progress'
 import { useGlobal } from '@/lib/global'
 
-/**
- * 目录导航组件
- * @param toc
- * @returns {JSX.Element}
- * @constructor
- */
 const Catalog = ({ toc }) => {
   const { locale } = useGlobal()
-  // 监听滚动事件
+
   useEffect(() => {
     window.addEventListener('scroll', actionSectionScrollSpy)
     actionSectionScrollSpy()
@@ -21,11 +15,8 @@ const Catalog = ({ toc }) => {
     }
   }, [])
 
-  // 目录自动滚动
   const tRef = useRef(null)
   const tocIds = []
-
-  // 同步选中目录事件
   const [activeSection, setActiveSection] = useState(null)
 
   const throttleMs = 200
@@ -42,13 +33,11 @@ const Catalog = ({ toc }) => {
       const bbox = section.getBoundingClientRect()
       const prevHeight = prevBBox ? bbox.top - prevBBox.bottom : 0
       const offset = Math.max(150, prevHeight / 4)
-      // GetBoundingClientRect returns values relative to viewport
       if (bbox.top - offset < 0) {
         currentSectionId = section.getAttribute('data-id')
         prevBBox = bbox
         continue
       }
-      // No need to continue loop, if last element has been detected
       break
     }
     setActiveSection(currentSectionId)
@@ -56,40 +45,39 @@ const Catalog = ({ toc }) => {
     tRef?.current?.scrollTo({ top: 28 * index, behavior: 'smooth' })
   }, throttleMs))
 
-  // 无目录就直接返回空
   if (!toc || toc.length < 1) {
     return <></>
   }
 
-  return <div className='px-3 py-1'>
-    <div className='w-full'><i className='mr-1 fas fa-stream' />{locale.COMMON.TABLE_OF_CONTENTS}</div>
-    <div className='w-full py-3'>
-      <Progress />
-    </div>
-    <div className='overflow-y-auto max-h-36 lg:max-h-96 overscroll-none scroll-hidden' ref={tRef}>
-      <nav className='h-full  text-black'>
-        {toc.map((tocItem) => {
-          const id = uuidToId(tocItem.id)
-          tocIds.push(id)
-          return (
-            <a
-              key={id}
-              href={`#${id}`}
-              className={`notion-table-of-contents-item duration-300 transform font-light dark:text-gray-200
-            notion-table-of-contents-item-indent-level-${tocItem.indentLevel} `}
-            >
-              <span style={{ display: 'inline-block', marginLeft: tocItem.indentLevel * 16 }}
-                className={`${activeSection === id && ' font-bold text-indigo-600'}`}
+  return (
+    <div className='fixed top-0 right-0 px-3 py-1'>
+      <div className='w-full'><i className='mr-1 fas fa-stream' />{locale.COMMON.TABLE_OF_CONTENTS}</div>
+      <div className='w-full py-3'>
+        <Progress />
+      </div>
+      <div className='overflow-y-auto max-h-36 lg:max-h-96 overscroll-none scroll-hidden' ref={tRef}>
+        <nav className='h-full text-black'>
+          {toc.map((tocItem) => {
+            const id = uuidToId(tocItem.id)
+            tocIds.push(id)
+            return (
+              <a
+                key={id}
+                href={`#${id}`}
+                className={`notion-table-of-contents-item duration-300 transform font-light dark:text-gray-200 notion-table-of-contents-item-indent-level-${tocItem.indentLevel}`}
               >
-                {tocItem.text}
-              </span>
-            </a>
-          )
-        })}
-      </nav>
-
+                <span style={{ display: 'inline-block', marginLeft: tocItem.indentLevel * 16 }}
+                  className={`${activeSection === id && ' font-bold text-indigo-600'}`}
+                >
+                  {tocItem.text}
+                </span>
+              </a>
+            )
+          })}
+        </nav>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default Catalog
