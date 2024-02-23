489d712995c0f53be7deb6e2eceb174eb49e9b05
JunLeoLiu <liujun2015@hotmail.com>
2024-02-23 13:58:50 +0800

    na

--- themes/ce/components/PostHeader.js
+++ themes/ce/components/PostHeader.js
@@ -20,7 +20,7 @@ export default function PostHeader({ post, siteInfo }) {
   const headerImage = post?.pageCover ? post.pageCover : siteInfo?.pageCover
 
   return (
-    <div id="header" className="w-full h-0 relative md:flex-shrink-0 z-39" >
+    <div id="header" className="w-full h-0 relative md:flex-shrink-0 z-10" >
       <header id='article-header-cover'
             className="absolute top-0 w-full h-40 py-2 flex justify-center items-center ">
 
