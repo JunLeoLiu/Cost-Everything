2f65887d568df4c0201d81321aae18a3e6cf82c2
JunLeoLiu <liujun2015@hotmail.com>
2024-02-23 16:08:11 +0800

    na

--- themes/ce/components/SideRight.js
+++ themes/ce/components/SideRight.js
@@ -76,7 +76,7 @@ export default function SideRight(props) {
 
       {siteConfig('COMMENT_WALINE_SERVER_URL') && siteConfig('COMMENT_WALINE_RECENT') && <HexoRecentComments/>}
 
-      <div className='fixed top-30 right-20'>
+      <div className='fixed top-30 right-50'>
         {post && post.toc && post.toc.length > 1 && <Catalog toc={post.toc} />}
 
         {rightAreaSlot}
