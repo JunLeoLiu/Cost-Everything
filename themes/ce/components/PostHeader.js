81e878352e7b26703537dd918a0b6513eca9c2db
JunLeoLiu <liujun2015@hotmail.com>
2024-02-23 09:49:40 +0800

    na

--- themes/ce/components/PostHeader.js
+++ themes/ce/components/PostHeader.js
@@ -20,17 +20,17 @@ export default function PostHeader({ post, siteInfo }) {
   const headerImage = post?.pageCover ? post.pageCover : siteInfo?.pageCover
 
   return (
-    <div id="header" className="w-full h-48 relative md:flex-shrink-0 z-10">
+    <div id="header" className="w-full h-30 fixed md:flex-shrink-0 z-50" >
       <header id='article-header-cover'
-            className="absolute top-0 w-full h-30 py-6 flex justify-center items-center ">
+            className="bg-black bg-opacity-70 absolute top-0 w-full h-30 py-6 flex justify-center items-center ">
 
         <div className='mt-10'>
           {/* 文章Title */}
-          <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center">
+          <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center text-white">
             <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />{post.title}
           </div>
 
-          <section className="flex-wrap shadow-text-md flex text-sm justify-center mt-4 dark:text-gray-400 font-light leading-8">
+          <section className="flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8">
 
             <div className='flex justify-center dark:text-gray-200 text-opacity-70'>
               {post?.type !== 'Page' && (
