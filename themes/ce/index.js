c706c119d2879a57c967798d5126d221ead5634a
JunLeoLiu <liujun2015@hotmail.com>
2024-02-23 12:11:08 +0800

    na

--- themes/ce/index.js
+++ themes/ce/index.js
@@ -234,8 +234,6 @@ const LayoutSlug = props => {
                     <article itemScope itemType="https://schema.org/Movie" className="subpixel-antialiased overflow-y-hidden" >
                         {/* Notion文章主体 */}
                         <section className='px-5 justify-center mx-auto max-w-2xl lg:max-w-full'>
-
-                            {post.title}
                             {post && <NotionPage post={post} />}
                         </section>
 
