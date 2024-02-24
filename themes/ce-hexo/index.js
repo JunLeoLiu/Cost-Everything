9bed8215baab5756c3fb3327e04049ae08b97e41
JunLeoLiu <liujun2015@hotmail.com>
2024-02-25 00:54:41 +0800

    na

--- themes/ce-hexo/index.js
+++ themes/ce-hexo/index.js
@@ -129,11 +129,6 @@ const LayoutBase = props => {
             {/* 悬浮菜单 */}
             <RightFloatArea floatSlot={floatSlot} />
 
-            {/* 右上角悬浮 */}
-            <div ref={floatButtonGroup} className='right-8 Top-30 lg:right-10 fixed justify-end z-20 '>
-                <Catalog />
-            </div>
-
             {/* 全文搜索 */}
             <AlgoliaSearchModal cRef={searchModal} {...props}/>
 
