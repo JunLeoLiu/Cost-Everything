30aa006557d2f023e8ecc1cbe783e2c3d600f36b
JunLeoLiu <liujun2015@hotmail.com>
2024-02-24 10:44:37 +0800

    na

--- themes/ce/components/PostHeader.js
+++ themes/ce/components/PostHeader.js
@@ -22,10 +22,9 @@ export default function PostHeader({ post, siteInfo }) {
 
   return (
     <div id="header" className="w-full h-40 relative md:flex-shrink-0 z-10" >
-      <LazyImage priority={true} src={headerImage} className='w-full h-full object-cover object-center absolute top-0'/>
 
       <header id='article-header-cover'
-            className="bg-black bg-opacity-70 absolute top-0 w-full h-40 py-10 flex justify-center items-center ">
+            className="absolute top-0 w-full h-40 py-10 flex justify-center items-center ">
 
         <div className='mt-10'>
             <div className='mb-3 flex justify-center'>
