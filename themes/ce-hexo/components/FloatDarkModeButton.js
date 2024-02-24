849c1b2373555e67f8c984d4f92bedc050878659
JunLeoLiu <liujun2015@hotmail.com>
2024-02-25 00:05:43 +0800

    na

--- themes/ce-hexo/components/FloatDarkModeButton.js
+++ themes/ce-hexo/components/FloatDarkModeButton.js
@@ -26,7 +26,7 @@ export default function FloatDarkModeButton () {
       className={'justify-center items-center w-7 h-7 text-center transform hover:scale-105 duration-200'
       }
     >
-      <i id="darkModeButton" className={`${isDarkMode ? 'fa-sun' : 'fa-moon'} fas text-xs`}/>
+      <i id="darkModeButton" className={`${isDarkMode ? 'fa-regular fa-sun' : 'fa-regular fa-moon'} fas text-xs`}/>
     </div>
   )
 }
