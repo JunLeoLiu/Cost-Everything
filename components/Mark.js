93243467d4edd1813058bdf26af0e35a60c0036f
JunLeoLiu <liujun2015@hotmail.com>
2024-02-26 10:03:13 +0800

    na

--- components/Mark.js
+++ components/Mark.js
@@ -1,10 +1,10 @@
 import { loadExternalResource } from '@/lib/utils'
 
 /**
- * 将搜索结果的关键词高亮，并显示文章摘要
+ * 将搜索结果的关键词高亮
  */
-export default async function replaceSearchResult({ doms, search, target, summaries }) {
-  if (!doms || !search || !target || !summaries) {
+export default async function replaceSearchResult({ doms, search, target }) {
+  if (!doms || !search || !target) {
     return
   }
 
@@ -12,24 +12,21 @@ export default async function replaceSearchResult({ doms, search, target, summar
     await loadExternalResource('https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js', 'js')
     const Mark = window.Mark
     if (doms instanceof HTMLCollection) {
-      for (let i = 0; i < doms.length; i++) {
-        const container = doms[i]
+      for (const container of doms) {
         const re = new RegExp(search, 'gim')
         const instance = new Mark(container)
         instance.markRegExp(re, target)
 
-        // Add summary to the result
-        const summary = summaries[i]
-        container.appendChild(document.createElement('p')).textContent = summary;
+        // Add summaries to each result
+        for (const summary of summaries) {
+          container.appendChild(document.createElement('p')).textContent = summary;
+          
+        }
       }
     } else {
       const re = new RegExp(search, 'gim')
       const instance = new Mark(doms)
       instance.markRegExp(re, target)
-
-      // Add summary to the result
-      const summary = summaries[0]
-      doms.appendChild(document.createElement('p')).textContent = summary;
     }
   } catch (error) {
     console.error('markjs 加载失败', error)
