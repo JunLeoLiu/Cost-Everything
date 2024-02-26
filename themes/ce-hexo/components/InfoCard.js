21770232eff170106079361e28bd9d10b157e9b3
JunLeoLiu <liujun2015@hotmail.com>
2024-02-26 12:08:32 +0800

    na

--- themes/ce-hexo/components/InfoCard.js
+++ themes/ce-hexo/components/InfoCard.js
@@ -22,7 +22,9 @@ export function InfoCard(props) {
           </div>
         </a>
       </div>
+      <></>
       <div className='font-medium text-center text-xl pb-4'>{siteConfig('AUTHOR')}</div>
+      <></>
       <div className='text-sm text-center'>{siteConfig('BIO')}</div>
       <MenuGroupCard {...props} />
       <SocialButton />
