2821704b85e917b218d2c6b826cb0ad14d79f197
JunLeoLiu <liujun2015@hotmail.com>
2024-02-26 09:59:57 +0800

    na

--- components/Mark.js
+++ components/Mark.js
@@ -1,10 +1,10 @@
 import { loadExternalResource } from '@/lib/utils'
 
 /**
- * 将搜索结果的关键词高亮
+ * 将搜索结果的关键词高亮，并显示文章摘要
  */
-export default async function replaceSearchResult({ doms, search, target }) {
-  if (!doms || !search || !target) {
+export default async function replaceSearchResult({ doms, search, target, summaries }) {
+  if (!doms || !search || !target || !summaries) {
     return
   }
 
@@ -12,15 +12,24 @@ export default async function replaceSearchResult({ doms, search, target }) {
     await loadExternalResource('https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js', 'js')
     const Mark = window.Mark
     if (doms instanceof HTMLCollection) {
-      for (const container of doms) {
+      for (let i = 0; i < doms.length; i++) {
+        const container = doms[i]
         const re = new RegExp(search, 'gim')
         const instance = new Mark(container)
         instance.markRegExp(re, target)
+
+        // Add summary to the result
+        const summary = summaries[i]
+        container.appendChild(document.createElement('p')).textContent = summary;
       }
     } else {
       const re = new RegExp(search, 'gim')
       const instance = new Mark(doms)
       instance.markRegExp(re, target)
+
+      // Add summary to the result
+      const summary = summaries[0]
+      doms.appendChild(document.createElement('p')).textContent = summary;
     }
   } catch (error) {
     console.error('markjs 加载失败', error)
