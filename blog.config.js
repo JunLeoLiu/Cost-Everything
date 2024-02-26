a99c4a487e551978b34a9c28eabf4dd08e5a59df
JunLeoLiu <liujun2015@hotmail.com>
2024-02-26 12:37:20 +0800

    na

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Cost Everything', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '零部件拆解及成本分析<br>邮件联系我们 CE@costeverything.com', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '零部件拆解及成本分析', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://costeverything.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Cost Everything, 零部件拆解和成本分析', // 网站关键词 英文逗号隔开
 
--- themes/ce-hexo/components/InfoCard.js
+++ themes/ce-hexo/components/InfoCard.js
@@ -24,7 +24,7 @@ export function InfoCard(props) {
       </div>
       <br></br>
       <div className='font-medium text-center text-xl pb-4'>{siteConfig('AUTHOR')}</div>
-      <br></br>
+      <div className='text-sm text-center'>邮件: CE@costeverything.com</div>
       <div className='text-sm text-center'>{siteConfig('BIO')}</div>
       <MenuGroupCard {...props} />
       <SocialButton />
