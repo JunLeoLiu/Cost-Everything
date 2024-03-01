d15bdb3c360ffb56c44d95e11491689d3283eec4
JunLeoLiu <liujun2015@hotmail.com>
2024-03-01 15:24:28 +0800

    na

--- themes/ce-hexo/components/InfoCard.js
+++ themes/ce-hexo/components/InfoCard.js
@@ -23,9 +23,9 @@ export function InfoCard(props) {
         </a>
       </div>
       <div className='font-medium text-center text-xl pb-4'>{siteConfig('AUTHOR')}</div>
-      <div className='text-sm text-center' style={{ marginBottom: '10px' }}>{siteConfig('BIO')}</div>
-      <div className='text-sm text-center' style={{ marginBottom: '10px' }}>E-mail: CE@costeverything.com</div>
-      <div className='text-sm text-center' style={{ marginBottom: '10px' }}>www.costeverything.com</div>
+      <div className='text-sm text-center' style={{ marginBottom: '5px' }}>{siteConfig('BIO')}</div>
+      <div className='text-sm text-center' style={{ marginBottom: '5px' }}>E-mail: CE@costeverything.com</div>
+      <div className='text-sm text-center' style={{ marginBottom: '5px' }}>www.costeverything.com</div>
       <MenuGroupCard {...props} />
       <SocialButton />
     </Card>
